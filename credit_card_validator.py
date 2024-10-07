fe5608bca9d48b6974de809a436708230eadc7bf
Raj Patel <rajpatel6@my.unt.edu>
2024-10-06 20:36:06 -0500

    Credit card

---
+++ credit_card_validator.py
@@ -0,0 +1,18 @@
+# let's go get some software that someone else wrote
+import luhn  
+
+# a function that uses the 'luhn' code - it takes a 'card_number' and returns 'true' or 'false' if the card is valid.
+def is_credit_card_valid(card_number):    
+    return luhn.verify(card_number)
+
+# Let's tell the world whose awful credit card validator this is.
+print("Raj Patel's Credit Card Validator")
+
+# Let's get the card number from the user
+card_number = input("Enter your 16-digit credit card number: ")
+
+# Validate the credit card number using the Luhn algorithm
+if is_credit_card_valid(card_number):           # we are calling the function above and sending it the card_number to validate
+    print("The credit card number is valid.")
+else:
+    print("The credit card number is invalid.")
